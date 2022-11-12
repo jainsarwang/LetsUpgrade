36102aec8bd422930002dd614aee35e0e980a752
Sarwang Jain <96441225+jainsarwang@users.noreply.github.com>
2022-11-12 20:44:39 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Arithmetic{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Two numbers: ");
+        double num1 = sc.nextDouble(), num2 = sc.nextDouble(), res = 0;
+
+        System.out.println("Choose an Operator: ");
+        char op = sc.next().charAt(0);
+
+        sc.close();
+        
+        switch(op){
+            case '+':
+                res = num1+num2;
+                break;
+            case '-':
+                res = num1-num2;
+                break;
+            case '*':
+                res = num1*num2;
+                break;
+            case '/':
+                res = num1/num2;
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
+
+        System.out.println(res);
+    }
+}
